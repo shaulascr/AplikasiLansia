6717649e1d5b5f7b072a1f11c592479d6accda93
shaulascr <alya.roosyidah@gmail.com>
2024-05-20 08:44:16 +0700

    First Commit

---
+++ app/src/main/java/com/alya/aplikasilansia/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.alya.aplikasilansia;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.ui.AppBarConfiguration;
+
+import com.alya.aplikasilansia.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+    }
+
+}
