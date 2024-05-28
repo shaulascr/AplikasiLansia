16043e7026a5b3acd0d4dd00cfca9672fb24d9e7
zulfa <zuhrahzlf@gmail.com>
2024-05-28 22:12:21 +0700

    layout news

--- app/src/main/java/com/alya/aplikasilansia/ui/profile/EditProfileActivity.java
+++ app/src/main/java/com/alya/aplikasilansia/ui/profile/EditProfileActivity.java
@@ -1,16 +1,25 @@
 package com.alya.aplikasilansia.ui.profile;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.alya.aplikasilansia.R;
 
 public class EditProfileActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        // Set click listener for the Cancel button
+        findViewById(R.id.btn_cancelProfile).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Finish the EditProfileActivity and return to the previous activity
+                finish();
+            }
+        });
     }
 }
